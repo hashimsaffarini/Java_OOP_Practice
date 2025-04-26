404992f4e04cb7450a76b3d94108497a03bfbf28
hashimsaffarini <hashimsaffarini044@gmail.com>
2025-04-26 17:47:47 +0300

    First Commit

---
+++ src/main/java/com/mycompany/java_oop_practice/Java_OOP_Practice.java
@@ -0,0 +1,11 @@
+
+
+package com.mycompany.java_oop_practice;
+
+
+public class Java_OOP_Practice {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
